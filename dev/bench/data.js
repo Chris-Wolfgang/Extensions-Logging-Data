22a791ecad5343333676af3b13747c111a317e1a
github-action-benchmark <github@users.noreply.github.com>
2026-06-12 01:55:54 +0000

    add BenchmarkDotNet (benchmarkdotnet) benchmark result for ac589db832c89a38a41eefbb2a201019f726e7c9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781223831266,
+  "lastUpdate": 1781229354443,
   "repoUrl": "https://github.com/Chris-Wolfgang/Extensions-Logging-Data",
   "entries": {
     "BenchmarkDotNet": [
@@ -42,6 +42,48 @@ window.BENCHMARK_DATA = {
             "value": 3281.5537338256836,
             "unit": "ns",
             "range": "± 51.78219206885666"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "210299580+Chris-Wolfgang@users.noreply.github.com",
+            "name": "Chris Wolfgang",
+            "username": "Chris-Wolfgang"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ac589db832c89a38a41eefbb2a201019f726e7c9",
+          "message": "Merge pull request #127 from Chris-Wolfgang/release/v0.1.1-prep\n\nRelease v0.1.1 prep: CHANGELOG entry + PublicAPI baseline",
+          "timestamp": "2026-06-11T21:54:21-04:00",
+          "tree_id": "56f86f4f3fafc9170bef059c6d7f7a5a5846359a",
+          "url": "https://github.com/Chris-Wolfgang/Extensions-Logging-Data/commit/ac589db832c89a38a41eefbb2a201019f726e7c9"
+        },
+        "date": 1781229353592,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Wolfgang.Extensions.Logging.Data.Benchmarks.DbConnectionLoggerExtensionsBenchmarks.LogDbConnection_DisabledLogger_FastPath",
+            "value": 0.0010442597170670827,
+            "unit": "ns",
+            "range": "± 0.0018087108862576882"
+          },
+          {
+            "name": "Wolfgang.Extensions.Logging.Data.Benchmarks.DbConnectionLoggerExtensionsBenchmarks.LogDbConnection_EnabledLogger_FullWork",
+            "value": 3258.8899167378745,
+            "unit": "ns",
+            "range": "± 8.83521924112885"
+          },
+          {
+            "name": "Wolfgang.Extensions.Logging.Data.Benchmarks.DbConnectionLoggerExtensionsBenchmarks.LogDbConnection_EnabledLogger_ExplicitDebugLevel",
+            "value": 3133.5257568359375,
+            "unit": "ns",
+            "range": "± 24.83513125600716"
           }
         ]
       }
